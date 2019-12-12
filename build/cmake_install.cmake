dea3a1f1ff6df865e89bb5d9a44c39edf4e34d78
prujulia <pru.julia@yandex.ru>
2019-12-12 23:54:28 +0300

    Added Cmake

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Skynet/Documents/Visual Studio 2015/UserInterface/UserInterface
+# Install script for directory: C:/Users/Skynet/Documents/Visual Studio 2015/UserInterface
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Project")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/UserInterface")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Skynet/Documents/Visual Studio 2015/UserInterface/UserInterface/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Skynet/Documents/Visual Studio 2015/UserInterface/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
